c47b0671402d0373fb5e02ef57460a9841945ff1
byronallan <127366720+byronallan@users.noreply.github.com>
2023-08-21 20:14:27 -0700

    got my prompt to work when I enter node index.js into command line

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const inquirer = require('inquirer');
 
 class SvgMaker {
-  async logoPrompt() {
+}
+function logoPrompt() {
     return inquirer.prompt([
-      {
+        {
         type: 'input',
         name: 'text',
         message: 'What text would you like to be entered for this logo (up to 3 characters max)?',
-      },
-      {
+        },
+        {
         type: 'input',
         name: 'textColor',
         message: 'Can you please enter the color that you would like to utilize for your text?',
-      },
-      {
+        },
+        {
         type: 'list',
         name: 'shape',
         message: 'Which of these shapes would you like your logo to conform to?',
         choices: ['Triangle', 'Square', 'Circle'],
-      },
+        },
     ]);
-  }
-}
+};
+
+logoPrompt().then(res => {
+    console.log(res);
+});
