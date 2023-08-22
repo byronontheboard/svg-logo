e324ddd9195ea6969e4c0d9ff86e42b78c70cd1a
byronallan <127366720+byronallan@users.noreply.github.com>
2023-08-21 20:48:03 -0700

    forgot to add a question as well as hex number option for colors

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ function logoPrompt() {
         {
         type: 'input',
         name: 'text',
-        message: 'What text would you like to be entered for this logo (up to 3 characters max)?',
+        message: 'What text would you like to be entered for this logo(up to 3 characters max)?',
         },
         {
         type: 'input',
         name: 'textColor',
-        message: 'Can you please enter the color that you would like to utilize for your text?',
+        message: 'Can you please enter the color that you would like to utilize for your text(hexadecimal numbers are accepted as well)?',
         },
         {
         type: 'list',
         name: 'shape',
         message: 'Which of these shapes would you like your logo to conform to?',
         choices: ['Triangle', 'Square', 'Circle'],
+        },
+        {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'In regards to the decided shape, would you please pick a color(hexadecimal numbers are accepted as well)?',
         },
     ]);
 };
